1ca05bcc000ebcf992416882e1bd5d6feaddbdc7
Ahmed Ali Khan <117421716+Ahmed-2003-khan@users.noreply.github.com>
2026-01-04 20:34:58 +0500

    Add email field to Student model

--- structured_output/pydantic_demo.py
+++ structured_output/pydantic_demo.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 
 class Student(BaseModel):
     name: str = 'Ahmed'
     age: Optional[int] = None
+    email: EmailStr
 
 
-new_student = {'age':'22'}
+new_student = {'age':'22', 'email':'abc'}
 
 student = Student(**new_student)
 
